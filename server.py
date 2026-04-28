94d62e34d507aa6013cf0f918881ab9abf664d27
Your ituzis <nikkice37@gmail.com>
2026-04-28 18:57:25 -0400

    fix env creds

--- server.py
+++ server.py
@@ -1,5 +1,25 @@
 from flask import Flask, request, jsonify
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+import json
+import os
+
+creds_dict = json.loads(os.environ["creds"])
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open_by_key("15_GGlx6HeQR-UjxWEcR0oBPBvgKamXozO-ANUveGPFw").sheet1
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,12 +29,21 @@ def home():
 @app.route('/submit', methods=['POST'])
 def receive_data():
     data = request.json
-    print("Received from Roblox:", data)
+    print("Received:", data)
 
-    return jsonify({
+    username = data.get("username")
+    goals = data.get("goals")
+
+    try:
+        sheet.append_row([username, goals])
+        print("SUCCESS writing to sheet")
+    except Exception as e:
+        print("SHEET ERROR:", e)
+
+    return {
         "status": "success",
-        "message": "Data received!"
-    })
+        "message": f"Added {username} with {goals} goals"
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
